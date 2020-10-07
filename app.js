6833ad81871e390360ef4ff0c7cbbb0d5b952d1c
randomuy1289 <ashutoshshenoy@gmail.com>
2020-10-07 10:56:32 +0530

    Carousel implementation
    Implemented a carousel with swiperJS

--- app.js
+++ app.js
@@ -7,3 +7,38 @@ togglebtn.onclick = () => {
     body.classList.toggle('light');
     body.classList.toggle('dark');
 };
+
+//SWIPER JS CODE STARTS HERE
+var mySwiper = new Swiper('.swiper-container', {
+      
+    slidesPerView: 4,
+    spaceBetween: 80,
+    loop: true,
+    navigation: {
+  nextEl: '.swiper-button-next',
+  prevEl: '.swiper-button-prev',
+},
+    /*pagination: {
+      //el: '.swiper-pagination',
+      clickable: true,
+    },*/
+})
+/*loop: true,
+
+// If we need pagination
+pagination: {
+  el: '.swiper-pagination',
+},
+
+// Navigation arrows
+navigation: {
+  nextEl: '.swiper-button-next',
+  prevEl: '.swiper-button-prev',
+},
+
+// And if we need scrollbar
+scrollbar: {
+  el: '.swiper-scrollbar',
+},*/
+
+//SWIPER JS CODE ENDS HERE
