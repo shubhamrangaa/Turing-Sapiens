45c41453d77b4173d7e39b7cd85914999693846d
Shubham Ranga <51476691+shubhamrangaa@users.noreply.github.com>
2020-11-13 18:25:55 +0530

    Member form makeover

--- member.js
+++ member.js
@@ -4,7 +4,7 @@ const proceed = document.querySelector('.go-to-level');
 const level2 = document.querySelector('.level-2');
 console.log("FROM MEMBER FORM.JS")
 proceed.onclick = () => {
-  proceed.classList.toggle('light');
+  proceed.style.display = "none";
   level2.classList.add('visible');
 };
 
