7d98e3fd28a1d8c5faf8a3b8f444201c7877f8e9
Shubham Ranga <51476691+shubhamrangaa@users.noreply.github.com>
2020-12-24 19:57:54 +0530

    Minor Enhancements

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 window.addEventListener("load", () => setTimeout(function(){
   const loader = document.querySelector('.loader');
   loader.classList.add('loaded');
-},2500));
+},2000));
 
 // PRELOADER ENDS
 
