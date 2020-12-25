abe9975e842bab9746af154563885e40efeb8d85
Shubham Ranga <51476691+shubhamrangaa@users.noreply.github.com>
2020-12-25 12:31:09 +0530

    Minor fix

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 window.addEventListener("load", () => setTimeout(function(){
   const loader = document.querySelector('.loader');
   loader.classList.add('loaded');
-},2000));
+},1800));
 
 // PRELOADER ENDS
 
